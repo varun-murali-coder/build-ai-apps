f1a70cb80631ac7827d504c5840e9bcbc5d56ac6
varunmurali <varunmurali95@gmail.com>
2026-07-25 10:02:05 +0530

    testing

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,23 +3,24 @@ import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
-const [message, setMessage] = useState('')
+   const [message, setMessage] = useState('');
    useEffect(() => {
       fetch('/api/hello')
          .then((res) => res.json())
-
 
          .then((data) => setMessage(data.message));
    }, []);
    return (
       <div className="p-4">
-   <p className="font-
-   bold text-3xl">{message}</p>
-                   <Button>Click
-                      Me</Button>
-
+         <p
+            className="font-
+   bold text-3xl"
+         >
+            {message}
+         </p>
+         <Button>Click Me</Button>
       </div>
-   )
+   );
 }
 
 export default App;
